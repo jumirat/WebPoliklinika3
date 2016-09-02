9c975c1fbc4738687a3ab940cadaa1c09b403cc5
jumirat <georgemirat3@yahoo.com>
2016-09-02 17:41:46 +0200

    Dependencies for angular and web app.

---
+++ src/WebPoliklinika3/gulpfile.js
@@ -0,0 +1,46 @@
+﻿var ts = require('gulp-typescript');
+var gulp = require('gulp');
+var clean = require('gulp-clean');
+var merge = require('merge-stream');
+
+var destPath = './wwwroot/js/';
+
+gulp.task('clean', function () {
+    return gulp.src(destPath)
+        .pipe(clean());
+});
+
+//var clientLibraries = [
+//    "core-js",
+//    "zone.js",
+//    "reflect-metadata",
+//    "systemjs",
+//    "@angular",
+//    "rxjs",
+//    "es6-shim"
+//];
+
+//gulp.task("copyClientDeps",
+//    function () {
+//        var mergeStream = merge();
+//        for (var i = 0; i < clientLibraries.length; i++) {
+//            mergeStream.add(gulp.src([destPath.nodeModules + clientLibraries[i] + "/**/*", '!' + destPath.nodeModules + clientLibraries[i] + "/**/*tsconfig.json"])
+//                .pipe(gulp.dest(destPath.clientDeps + clientLibraries[i])));
+//        }
+//        return mergeStream;
+//    });
+
+gulp.task("copyClientDeps", () => {
+    gulp.src([
+        "core-js",
+        "zone.js",
+        "reflect-metadata",
+        "systemjs",
+        "@angular",
+        "rxjs",
+        "es6-shim"
+    ], {
+        cwd: "node_modules/**"
+    })
+        .pipe(gulp.dest(destPath));
+});
